bd0328c67a84c47e994031c1a500e8278bf42ae2
USER <USER@DESKTOP-FKF169D>
2022-08-12 15:10:29 +0530

    commit 3

--- src/practice/PracticeProgram.java
+++ src/practice/PracticeProgram.java
@@ -1,5 +1,13 @@
 package practice;
 
 public class PracticeProgram {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hii this is first line");
+		System.out.println("Hii this is second line");
+		System.out.println("Hii this is third line");
+		
+	}
 
 }
